774dc872d0d1371ab821f7a3facc5af212dd23e3
Marcos Ramos <marcos.ramos@gmail.com>
2012-11-14 20:55:53 +0000

    ADD info when available and when not

--- check_google_nexus_availability.pl
+++ check_google_nexus_availability.pl
@@ -11,19 +11,34 @@ my $debug = 1;
 my $api_key = '';
 # Im only checking the ones I want
 my $url = {
-    nexus_4_8gb => 'https://play.google.com/store/devices/details?id=nexus_4_8gb',
-    nexus_4_16gb => 'https://play.google.com/store/devices/details?id=nexus_4_16gb',
-    nexus_10_16gb => 'https://play.google.com/store/devices/details?id=nexus_10_16gb',
+    'Nexus 4 8GB'   => 'https://play.google.com/store/devices/details?id=nexus_4_8gb',
+    'Nexus 4 16GB'  => 'https://play.google.com/store/devices/details?id=nexus_4_16gb',
+    'Nexus 10 16GB' => 'https://play.google.com/store/devices/details?id=nexus_10_16gb',
 };
 
 
 my $mech = WWW::Mechanize->new( cookie_jar => {} );
 $mech->agent_alias('Windows Mozilla');
 foreach my $version (keys %{$url} ) {
-    print 'URL => ' . $url->{$version} . "\n";
-#    $mech->get($url->{$version}) or die;
-#    my $html = $mech->content;
-#    version_available($version) if ( $html =~ /agotado/i );
+    print 'URL => ' . $url->{$version} . "\n" if $debug;
+    $mech->get($url->{$version}) or die;
+    my $html = $mech->content;
+    if ( $html =~ /agotado/i ) {
+        versioni_not_available($version);
+    } else {
+        version_available($version);
+    }
+}
+
+sub version_not_available {
+    my $version = shift;
+    # notify here
+    my $ws = WebService::Prowl->new(apikey => $api_key);
+    $ws->verify || die $ws->error();
+    $ws->add(   application => "Google Nexus Notif",
+                event       => $version,
+                description => "Still nothing..." );
+    print $version . "\n" if $debug;
 }
 
 sub version_available {
@@ -33,6 +48,6 @@ sub version_available {
     $ws->verify || die $ws->error();
     $ws->add(   application => "Google Nexus Notif",
                 event       => $version,
-                description => "Still nothing..." );
-    print $version if $debug;
+                description => "IT'S ALIVE!" );
+    print $version . "\n" if $debug;
 }
